463a886a1dfde5b59d80888d4c0ece01b2d815ab
tiagotrigo <tiagotrigo@bleu.com.br>
2019-09-20 18:21:24 -0300

    up

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ class Hades {
   }
 
   percent(price1, price2) {
-    return ((price1-price2)/price1)*100;
+    return ((price1 - price2) / price1) * 100;
   }
 
   async main() {
@@ -47,37 +47,30 @@ class Hades {
     const BL_BTC_USDT = await Bleutrade.getTicker('BTC_USDT');
     const BT_USDT_BRL = await Bitrecife.getTicker('USDT_BRL');
     const BT_BTC_BRL = await Bitrecife.getTicker('BTC_BRL');
-    
+
     // Book
     const BL_BOOK_BTC_USDT = await Bleutrade.getOrderBook('BTC_USDT', 'ALL', 5);
     const BT_BOOK_USDT_BRL = await Bitrecife.getOrderBook('USDT_BRL', 'ALL', 5);
     const BT_BOOK_BTC_BRL = await Bitrecife.getOrderBook('BTC_BRL', 'ALL', 5);
+      
+    // Quantity BRL to USDT
+    this.usdt = (BT_BRL.data.result[0].Balance / BT_BOOK_USDT_BRL.data.result.sell[0].Rate) * (1 - this.rateBt);
     
-    // Buy USDT
-    if (BT_BRL.data.result[0].Balance > 0) {
-      // await Bitrecife.setSellLimit('USDT_BRL',  BT_BOOK_USDT_BRL.data.result.sell[0].Rate, BT_BRL.data.result[0].Balance);
-      this.usdt = (BT_BRL.data.result[0].Balance / BT_BOOK_USDT_BRL.data.result.sell[0].Rate) * (1 - this.rateBt);
-    }
     // Ask
     // Quantity USDT to BTC(BL)
     this.qnt_BTC = (this.usdt / BL_BTC_USDT.data.result[0].Ask) * (1 - this.rateBl);
 
     // Bid
     // Quantity BTC to BRL(BT)
-    this.qnt_BRL = (this.qnt_BTC * (BT_BTC_BRL.data.result[0].Bid / BT_USDT_BRL.data.result[0].Bid)) * (1 - this.rateBt);
+    this.qnt_BRL = (this.qnt_BTC * BT_BTC_BRL.data.result[0].Bid) * (1 - this.rateBt);
 
     // Profit to arbitration
-    const profit = this.percent(this.qnt_BRL, this.usdt);
+    const profit = this.qnt_BRL - 25 
     
     if (profit > 1) {
       console.log('Arbitragem');
     } else {
-      console.log('=== BRL-USDT-BTC ===');
-      console.log(' ');
-      console.log('Entry:', this.usdt);
-      console.log('Price:', this.usdt + (this.usdt * (profit/100)));
-      console.log('Profit:', profit);
-      console.log(' ');
+      console.log(profit);
     }
   }
 

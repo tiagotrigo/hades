c0860a3d97c6499119b826a786dc550b6f1f5034
tiagotrigo <tiagotrigo@bleu.com.br>
2019-09-20 14:21:04 -0300

    Primeira versao do bot

--- app.js
+++ app.js
@@ -4,59 +4,94 @@ const R = require('ramda');
 const await = require('await');
 const Bitrecife = require('./bitrecife.js');
 const Bleutrade = require('./bleutrade.js');
+const sprintf = require("sprintf-js").sprintf;
 
 class Hades {
-	
-	constructor() {
-		this.feeMakerBl = 0.0025,
-		this.feeTakerBl = 0.0025,
-		this.feeMakerBt = 0.0020,
-		this.feeTakerBt = 0.0040,
-		this.brl = 25,
-		this.usd = 0.0
-	}
-	// se colocar uma ordem no book é maker
-	// se pegar uma ordem no livro é taker
+  
+  constructor() {
+    this.rateBl = 0.0025,
+    this.rateBt = 0.0040,
+    this.usdt = 0,
+    this.qnt_BRL = 0,
+    this.qnt_BTC = 0
+  }
 
-	wait(ms) {
-		return new Promise((resolve) => {
-			setTimeout(resolve, ms);
-		});
-	}
+  wait(ms) {
+    return new Promise((resolve) => {
+      setTimeout(resolve, ms);
+    });
+  }
 
-	repeat(ms, func) {
-		return new Promise((resolve) => (
-			setInterval(func, ms), 
-			this.wait(ms).then(resolve)
-		));
-	}
+  repeat(ms, func) {
+    return new Promise((resolve) => (
+      setInterval(func, ms), 
+      this.wait(ms).then(resolve)
+    ));
+  }
 
-	async main() {
-		const BL_BTC_USDT = await Bleutrade.getTicker('BTC_USDT');
-		const BT_USDT_BRL = await Bitrecife.getTicker('USDT_BRL');
+  qnt(amount, price) {
+    return amount * price;
+  }
 
-		// Trocando BRL por USDT
-		// pega a primeira ordem do bid
-		const val_USDT = this.brl / BT_USDT_BRL.data.result[0].Bid;
-		const val_USDT_FEE = val_USDT - (val_USDT * this.feeMakerBt);
-		// Trocando USDT por BTC
-		// pegar a primera ordem do ask
+  percent(price1, price2) {
+    return ((price1-price2)/price1)*100;
+  }
 
-		const val_BTC = val_USDT / BL_BTC_USDT.data.result[0].Ask;
-		const val_BTC_FEE = val_BTC - (val_BTC * this.feeMakerBl);
-		//
-		console.log('Quantidade em USDT:', val_USDT_FEE);
-		console.log('Quantidade em BTC:', val_BTC);
-	}
+  async main() {
+    // Balance
+    const BT_USDT = await Bitrecife.getBalance('USDT');
+    const BT_BRL = await Bitrecife.getBalance('BRL');
+    const BL_BTC = await Bitrecife.getBalance('BTC');
+    
+    // Ticker
+    const BL_BTC_USDT = await Bleutrade.getTicker('BTC_USDT');
+    const BT_USDT_BRL = await Bitrecife.getTicker('USDT_BRL');
+    const BT_BTC_BRL = await Bitrecife.getTicker('BTC_BRL');
+    
+    // Book
+    const BL_BOOK_BTC_USDT = await Bleutrade.getOrderBook('BTC_USDT', 'ALL', 5);
+    const BT_BOOK_USDT_BRL = await Bitrecife.getOrderBook('USDT_BRL', 'ALL', 5);
+    const BT_BOOK_BTC_BRL = await Bitrecife.getOrderBook('BTC_BRL', 'ALL', 5);
+    
+    // Buy USDT
+    if (BT_BRL.data.result[0].Balance > 0) {
+      // await Bitrecife.setSellLimit('USDT_BRL',  BT_BOOK_USDT_BRL.data.result.sell[0].Rate, BT_BRL.data.result[0].Balance);
+      this.usdt = (BT_BRL.data.result[0].Balance / BT_BOOK_USDT_BRL.data.result.sell[0].Rate) * (1 - this.rateBt);
+    }
+    // Ask
+    // Quantity USDT to BTC(BL)
+    this.qnt_BTC = (this.usdt / BL_BTC_USDT.data.result[0].Ask) * (1 - this.rateBl);
 
-	async run() {
-		this.repeat(
-			5000, 
-			() => Promise.all([
-				this.main()
-			])
-		);
-	}
+    // Bid
+    // Quantity BTC to BRL(BT)
+    this.qnt_BRL = (this.qnt_BTC * (BT_BTC_BRL.data.result[0].Bid / BT_USDT_BRL.data.result[0].Bid)) * (1 - this.rateBt);
+
+    // Profit to arbitration
+    const profit = this.percent(this.qnt_BRL, this.usdt);
+    
+    if (profit > 1) {
+      console.log('Arbitragem');
+    } else {
+      console.log('=== BRL-USDT-BTC ===');
+      console.log(' ');
+      console.log('Entry:', this.usdt);
+      console.log('Profit:', this.usdt + (this.usdt * (profit/100)));
+      console.log(' ');
+      console.log('BRL:', BT_BRL.data.result[0].Balance);
+      console.log('USDT:', BT_USDT.data.result[0].Balance);
+      console.log('BTC:', BL_BTC.data.result[0].Balance);
+      console.log(' ');
+    }
+  }
+
+  async run() {
+    this.repeat(
+      5000, 
+      () => Promise.all([
+        this.main()
+      ])
+    );
+  }
 }
 
 new Hades().run();

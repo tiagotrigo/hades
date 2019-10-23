e04d3c03e81511f4a4dd638243c0c2c3a40978fa
tiagotrigo <tiagotrigo@bleu.com.br>
2019-10-23 17:43:37 -0300

    up

--- app.js
+++ app.js
@@ -36,6 +36,17 @@ class Hades {
   }
 
   marketBTC(symbol, dividend) {
+    
+  }
+
+  setup() {
+    if (this.count >= Coins.length) this.count = 0;
+
+    const { 
+      symbol,
+      dividend
+    } = Coins[this.count];
+
     // Saldo na Exc
     Exc.getBalance('BTC').then((data) => {
       this.entry = data.data.result[0].Balance;
@@ -93,133 +104,6 @@ class Hades {
         });
       });
     });
-  }
-
-  marketUSDT(symbol, dividend) {
-    // Saldo na Exc
-    Exc.getBalance('BTC').then((data) => {
-      this.entry = data.data.result[0].Balance;
-      
-      // Vender BTC
-      Bleutrade.getOrderBook('BTC_USDT', 'ALL', 3).then((data) => {
-        this.gain = this.min;
-        const qntBidBleu = this.formatNumber((this.gain * data.data.result.buy[0].Rate) * (1 - 0.0025), 8);
-        
-        // Comprar NBC, ETH e LTC
-        Bleutrade.getOrderBook(symbol, 'ALL', 3).then((bookBleuUSDT) => {
-          const qntAskBleu = qntBidBleu * 0.9975;
-          const qntAskFee = this.formatNumber(qntAskBleu, 8) / bookBleuUSDT.data.result.sell[0].Rate;
-          const qntAskBleu_float = this.formatNumber(qntAskFee, 8);
-
-          // Vender NBC, ETH e LTC
-          Bleutrade.getOrderBook(`${dividend}_BTC`, 'ALL', 3).then((bookBleuBTC) => {
-            const qntBidBTC = this.formatNumber((qntAskBleu_float * bookBleuBTC.data.result.buy[0].Rate) * (1 - 0.0025), 8);
-            
-            if (qntBidBTC > this.gain) {
-              console.log(' ');
-              console.log('['+ symbol +']:', qntBidBTC);
-              console.log('Ganho:', qntBidBTC);
-              console.log(' ');
-            } else {
-              console.log('['+ symbol +']:', qntBidBTC);
-            }
-            
-          });
-        });
-      });
-    });
-  }
-
-  marketETH(symbol, dividend) {
-    // Saldo na Exc
-    Exc.getBalance('BTC').then((data) => {
-      this.entry = data.data.result[0].Balance;
-      
-      // Comprar ETH
-      Bleutrade.getOrderBook('ETH_BTC', 'ALL', 3).then((bookBleu) => {
-        this.gain = this.min;
-        const qntAskBleu = this.gain * 0.9975;
-        const qntAskFee = this.formatNumber(qntAskBleu, 8) / bookBleu.data.result.sell[0].Rate;
-        const qntAskBleu_float = this.formatNumber(qntAskFee, 8);
-        
-        // Comprar NBC, BCH e DASH
-        Bleutrade.getOrderBook(symbol, 'ALL', 3).then((bookBleuETH) => {
-          const qntAskETHBleu = qntAskBleu_float * 0.9975;
-          const qntAskETHFee = this.formatNumber(qntAskETHBleu, 8) / bookBleuETH.data.result.sell[0].Rate;
-          const qntAskETHBleu_float = this.formatNumber(qntAskETHFee, 8);
-          
-          // Vender NBC, BCH e DASH
-          Bleutrade.getOrderBook(`${dividend}_BTC`, 'ALL', 3).then((bookBleuBTC) => {
-            const qntBidBTC = this.formatNumber((qntAskETHBleu_float * bookBleuBTC.data.result.buy[0].Rate) * (1 - 0.0025), 8);
-            
-            if (qntBidBTC > this.gain) {
-              if (bookBleu.data.result.sell[0].Quantity >= qntAskBleu_float && bookBleuETH.data.result.sell[0].Quantity >= qntAskETHBleu_float) {
-                //Transferir Exc para Bleu
-                console.log(qntBidBTC)
-                Exc.setDirectTransfer('BTC', this.gain, 1, 'tiago.a.trigo@gmail.com').then((data) => {
-                  console.log('Enviando BTC para Bleutrade');
-                  // Comprar na Bleu
-                  Bleutrade.setBuyLimit('ETH_BTC', bookBleu.data.result.sell[0].Rate, qntAskBleu_float, false).then((data) => {
-                    console.log(`Troca de BTC por ${dividend}`);                    
-                    
-                    Bleutrade.setBuyLimit(symbol, bookBleuETH.data.result.sell[0].Rate, qntAskETHBleu_float, false).then((data) => {
-                      console.log(`Troca de ETH por ${dividend}`);
-                      // Vender na Exc
-                      Bleutrade.getBalance(dividend).then((data) => {
-                        const balance_1 = data.data.result[0].Balance;
-
-                        Bleutrade.setSellLimit(symbol, bookBleuBTC.data.result.buy[0].Rate, balance_1, false).then((data) => {
-                          console.log(`Trocar de ${dividend} por BTC`);
-
-                          Bleutrade.getBalance('BTC').then((data) => {
-                            const balance_2 = data.data.result[0].Balance;
-                            
-                            Bleutrade.setDirectTransfer('BTC', balance_2, 2, 'tiago.a.trigo@gmail.com').then((data) => {
-                              console.log('Enviando BTC para Excripto');
-                              process.exit();
-                            });
-                          });
-                        });
-                      });                      
-                    }); 
-                  })
-                });
-                //
-                console.log(' ');
-              } else {
-                console.log(' ');
-                console.log('Status: A primeira ordem do livro está inferior ao meu saldo');
-                console.log('Moeda:', symbol)
-                console.log('Ganho:', qntBidBTC);
-                console.log('Livro Bleutrade ETH:', this.formatNumber((bookBleuETH.data.result.buy[0].Quantity * bookBleuETH.data.result.buy[0].Rate), 8));
-                console.log('Livro Bleutrade BTC:', this.formatNumber((bookBleuBTC.data.result.sell[0].Quantity * bookBleuBTC.data.result.sell[0].Rate), 8));
-                console.log(' ');
-              }
-            } else {
-              console.log('['+ symbol +']:', qntBidBTC);
-            }
-            
-          });
-        });
-      });
-    });
-  }
-
-  setup() {
-    if (this.count >= Coins.length) this.count = 0;
-
-    const { 
-      symbol,
-      dividend
-    } = Coins[this.count];
-
-    if (Coins[this.count].market === 'BTC') {
-      this.marketBTC(symbol, dividend);
-    } else if (Coins[this.count].market === 'ETH') {
-      this.marketETH(symbol, dividend);
-    } else if (Coins[this.count].market === 'USDT') {
-      this.marketUSDT(symbol, dividend);
-    }
 
     this.count += 1;
   }
--- coins.js
+++ coins.js
@@ -2,49 +2,14 @@
 
 const Coins = [
 	{
-		symbol: 'LTC_BTC', 
-		dividend: 'LTC',
-		market: 'BTC'     
-	},
-	{
 		symbol: 'ETH_BTC', 
 		dividend: 'ETH',
 		market: 'BTC'
 	},
 	{
-		symbol: 'ETH_USDT', 
-		dividend: 'ETH',
-		market: 'USDT'
-	},
-	{
-		symbol: 'LTC_USDT', 
+		symbol: 'LTC_BTC', 
 		dividend: 'LTC',
-		market: 'USDT'
-	},
-	{
-		symbol: 'BCH_USDT', 
-		dividend: 'BCH',
-		market: 'USDT'
-	},
-	{
-		symbol: 'NBC_USDT', 
-		dividend: 'NBC',
-		market: 'USDT'
-	},
-	{
-		symbol: 'NBC_ETH', 
-		dividend: 'NBC',
-		market: 'ETH'
-	},
-	{
-		symbol: 'BCH_ETH', 
-		dividend: 'BCH',
-		market: 'ETH'
-	},
-	{
-		symbol: 'DASH_ETH', 
-		dividend: 'DASH',
-		market: 'ETH'
+		market: 'BTC'     
 	}
 ];
 

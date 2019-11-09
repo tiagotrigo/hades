947ba6c5665d94ec764818042a1d345c06530a19
tiagotrigo <tiagotrigo@localhost.localdomain>
2019-11-09 13:50:55 -0300

    up

--- bitrecife_bleutrade.js
+++ bitrecife_bleutrade.js
@@ -342,9 +342,11 @@ class Hades {
 
                 if (walks[walks.length - 1].exchangeto != 1) {
                   wallet = await walks[walks.length - 1].exchange.getBalance(walk.transfer.asset);
-
-                  await walk.exchange.setDirectTransfer(walks[walks.length - 1].transfer.asset, wallet.data.result[0].Available, 1, walks[walks.length - 1].transfer.mail);
-                  console.log(`Reenviando o restinho de ${walks[walks.length - 1].transfer.asset} para Bleutrade`);
+                  
+                  if (wallet.data.result[0].Available > 0.0001) {                  
+                    await walk.exchange.setDirectTransfer(walks[walks.length - 1].transfer.asset, wallet.data.result[0].Available, 1, walks[walks.length - 1].transfer.mail);
+                    console.log(`Reenviando o restinho de ${walks[walks.length - 1].transfer.asset} para Bleutrade`);
+                  }
                 }
               } 
             }

c5cc00d9f58b453ebca68d859f34a39cfa60c710
tiagotrigo <tiagotrigo@localhost.localdomain>
2019-11-12 16:40:29 -0300

    up

--- coins.js
+++ coins.js
@@ -2,6 +2,21 @@
 
 const Coins = [
 	{
+		symbol: 'ETH_USDT', 
+		dividend: 'ETH',
+		divisor: 'USDT'
+	},
+	{
+		symbol: 'LTC_USDT', 
+		dividend: 'LTC',
+		divisor: 'USDT'
+	},
+	{
+		symbol: 'BTC_USDT', 
+		dividend: 'BTC',
+		divisor: 'USDT'
+	},
+	{
 		symbol: 'ETH_BTC', 
 		dividend: 'ETH',
 		divisor: 'BTC'

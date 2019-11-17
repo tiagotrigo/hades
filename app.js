58a5b37f5f1c6ba4f0af9e7b304ac797e7804b81
tiagotrigo <tiagotrigo@localhost.localdomain>
2019-11-17 11:13:35 -0300

    up

--- app.js
+++ app.js
@@ -76,7 +76,7 @@ class Hades {
           let bleu = await Bleutrade.getOrderBook(symbol, 'BUY', 3);
           let bleuCalcQnt = this.calcSell(excCalcQnt, bleu.buy[0].Rate, 0.0015);
           // Lucro
-          if (bleuCalcQnt > (divisor === 'USDT' ? entry : 0.00020015) && bleu.buy[0].Rate > exc.sell[0].Rate) {
+          if (bleuCalcQnt > (divisor === 'USDT' ? entry : 0.0002) && bleu.buy[0].Rate > exc.sell[0].Rate) {
             await Exc.setBuyLimit(symbol, excCalcSum, excCalcQnt);
             console.log(`Troca de ${divisor} por ${dividend}`);
                               

ca3b3a60583c2063ca3cc8473efc5287cc8915b0
tiagotrigo <tiagotrigo@bleu.com.br>
2019-12-18 14:04:29 -0300

    LTC

--- arbitration.js
+++ arbitration.js
@@ -87,7 +87,7 @@ const Arbitration = [
   },
   {
     name: 'LTC_BTC',
-    entry: 0.0035,
+    entry: 0.0015,
     walks: [
       {
         exchangeto: 2,
@@ -127,7 +127,7 @@ const Arbitration = [
   },
   {
     name: 'LTC_BTC',
-    entry: 0.0035,
+    entry: 0.0015,
     walks: [
       {
         exchangeto: 1,

9b889811916a6b3baa6576d6799e281e2edad112
tiagotrigo <tiagotrigo@bleu.com.br>
2020-07-13 18:01:46 -0300

    Mudando mais uma vez o calculo para quantidade e resultado final

--- app.js
+++ app.js
@@ -64,7 +64,6 @@ class Hades {
     await Bullgain.setDirectTransfer(walk.receive.asset, entry, walk.receive.exchangeto, walk.receive.mail);
     console.log(`Enviando ${walk.receive.asset} (${entry}) da Bullgain para ${exchangeto}`);
   }
-
   // Atualizando o preço, procurando a melhor oferta
   async calcUpdateRate(walk, quantity) {
     let sum = 0;
@@ -85,228 +84,73 @@ class Hades {
 
     return price;
   }
-
   // Primeiro passo
   async calcQntOutput(arb) {
     let sum = 0;
-    let old = 0;
-    let calc = 0;
     let price = 0;
 
-    for (let [i, walk] of arb.walks.entries()) {
+    for (let [i, walk] of arb.walks.entries()) {      
       // Livro de ofertas
       let book = await walk.exchange.getOrderBook(walk.symbol, 'ALL', 3);
       // Verificando a ação 
       let orders = walk.action === 'buy' ? book.sell : book.buy;
-      // Primeiro passo
-      if (i === 0) {
-        // Se for BTC ou outra moeda como quote e base for igual a BRL ou USDT
-        // Calcular, com quantos USD posso comprar BTC ou outro moeda
-        if (walk.base === 'BRL' || walk.base === 'USDT') {
-          // Se for venda, calcule uma venda, senão calcule uma compra
-          if (walk.action === 'sell') {
-            for (let [s, order] of orders.entries()) {
-              sum = sum + order.Quantity;
-              walk.quantity = this.calcQntSell(arb.entry, order.Rate, walk.fee);
-              if (walk.quantity <= sum) {
-                walk.price = order.Rate;
-                break;
-              }
-            }
-          } else {
-            for (let [b, order] of orders.entries()) {
-              sum = sum + order.Quantity;
-              walk.quantity = this.calcQntBuy(arb.entry, order.Rate, walk.fee);
-              if (walk.quantity <= sum) {
-                walk.price = order.Rate;
-                break;
-              }
-            }
-          }
-        } else if (walk.quote === 'BRL' || walk.quote === 'USDT') {
-          // Se for venda, calcule uma venda, senão calcule uma compra
-          if (walk.action === 'sell') {
-            for (let [s, order] of orders.entries()) {
-              sum = sum + order.Quantity;
-              walk.quantity = arb.entry
-              if (walk.quantity <= sum) {
-                walk.price = order.Rate;
-                break;
-              }
-            }
-          } else {
-            for (let [b, order] of orders.entries()) {
-              sum = sum + order.Quantity;
-              walk.quantity = arb.entry;
-              if (walk.quantity <= sum) {
-                walk.price = order.Rate;
-                break;
-              }
-            }
-          }
+
+      price = i === 0 ? arb.entry : arb.walks[i - 1].quantity;
+
+      for (let [s, order] of orders.entries()) {
+        if (order.Rate >= 1) {
+          sum = sum + (order.Quantity * order.Rate);
         } else {
-          // Se for venda, calcule uma venda, senão calcule uma compra
+          sum = sum + order.Quantity;
+        }
+        
+        if (i > 0 && walk.quote === arb.walks[i - 1].quote) {
+          walk.quantity = this.mask(arb.walks[i - 1].quantity, 8);
+        } else {
           if (walk.action === 'sell') {
-            for (let [s, order] of orders.entries()) {
-              sum = sum + order.Quantity;
-              walk.quantity = this.calcQntSell(arb.entry, order.Rate, walk.fee);
-              if (walk.quantity <= sum) {
-                walk.price = order.Rate;
-                break;
-              }
-            }
+            walk.quantity = this.mask(this.calcQntSell(price, order.Rate, walk.fee), 8);
           } else {
-            for (let [b, order] of orders.entries()) {
-              sum = sum + order.Quantity;
-              walk.quantity = this.calcQntBuy(arb.entry, order.Rate, walk.fee);
-              if (walk.quantity <= sum) {
-                walk.price = order.Rate;
-                break;
-              }
-            }
+            walk.quantity = this.mask(this.calcQntBuy(price, order.Rate, walk.fee), 8);
           }
         }
-      // Se o quote da primeira for o mesmo da segunda, logo usaremos a quantidade do primeiro calculo
-      } else if (i === 1 && arb.walks[0].quote === walk.quote) {
-        // Se for venda, calcule uma venda, senão calcule uma compra
-        if (walk.action === 'sell') {
-          for (let [s, order] of orders.entries()) {
-            sum = sum + order.Quantity;
-            walk.quantity = arb.walks[0].quantity;
-            if (walk.quantity <= sum) {
-              walk.price = order.Rate;
-              break;
-            }
-          }
-        } else {
-          for (let [b, order] of orders.entries()) {
-            sum = sum + order.Quantity;
-            walk.quantity = arb.walks[0].quantity;
-            if (walk.quantity <= sum) {
-              walk.price = order.Rate;
-              break;
-            }
-          }
+
+        if (walk.quantity <= sum) {          
+          walk.price = order.Rate;
+          break;
         }
-      } else {
-        // Se for BTC ou outra moeda como quote e base for igual a BRL ou USDT
-        // Calcular, com quantos USD posso comprar BTC ou outro moeda
-        if (walk.base === 'BRL' || walk.base === 'USDT') {
-          // Se for venda, calcule uma venda, senão calcule uma compra
-          if (walk.action === 'sell') {
-            for (let [s, order] of orders.entries()) {
-              sum = sum + order.Quantity;
-              if (arb.walks[i - 1].action === 'sell') {
-                old = this.calcQntSell(arb.walks[i - 1].quantity, arb.walks[i - 1].price, arb.walks[i - 1].fee);
-              } else {
-                old = this.calcQntBuy(arb.walks[i - 1].quantity, arb.walks[i - 1].price, arb.walks[i - 1].fee);
-              }
-              walk.quantity = this.calcQntSell(old, order.Rate, walk.fee);
-              if (walk.quantity <= sum) {
-                walk.price = order.Rate;
-                break;
-              }
-            }
-          } else {
-            for (let [b, order] of orders.entries()) {
-              sum = sum + order.Quantity;
-              if (arb.walks[i - 1].action === 'sell') {
-                old = this.calcQntSell(arb.walks[i - 1].quantity, arb.walks[i - 1].price, arb.walks[i - 1].fee);
-              } else {
-                old = this.calcQntBuy(arb.walks[i - 1].quantity, arb.walks[i - 1].price, arb.walks[i - 1].fee);
-              }
-              walk.quantity = this.calcQntBuy(old, order.Rate, walk.fee);
-              if (walk.quantity <= sum) {
-                walk.price = order.Rate;
-                break;
-              }
-            }
-          }
-        } else if (walk.quote === 'BRL' || walk.quote === 'USDT') {
-          // Se for venda, calcule uma venda, senão calcule uma compra
-          if (walk.action === 'sell') {
-            for (let [s, order] of orders.entries()) {
-              sum = sum + order.Quantity;
-              walk.quantity = arb.walks[i - 1].quantity;
-              if (walk.quantity <= sum) {
-                walk.price = order.Rate;
-                break;
-              }
-            }
-          } else {
-            for (let [b, order] of orders.entries()) {
-              sum = sum + order.Quantity;
-              walk.quantity = arb.walks[i - 1].quantity;
-              if (walk.quantity <= sum) {
-                walk.price = order.Rate;
-                break;
-              }
-            }
-          }
-        } else {
-          // Se for venda, calcule uma venda, senão calcule uma compra
-          if (arb.walks[i - 1].action === 'sell') {
-            for (let [s, order] of orders.entries()) {
-              sum = sum + order.Quantity;
-              walk.quantity = this.calcQntSell(arb.walks[i - 1].quantity, order.Rate, walk.fee);
-              if (walk.quantity <= sum) {
-                walk.price = order.Rate;
-                break;
-              }
-            }
-          } else {
-            for (let [b, order] of orders.entries()) {
-              sum = sum + order.Quantity;
-              walk.quantity = this.calcQntBuy(arb.walks[i - 1].quantity, order.Rate, walk.fee);
-              if (walk.quantity <= sum) {
-                walk.price = order.Rate;
-                break;
-              }
-            }
-          }
-        }
-      }      
+      }
     }
-  }
-  
+  }  
   // Segundo passo
   async calcProfitOutput(arb) {
     const { walks } = arb;
 
-    let sum = 0;
     let output = 0;
+    let sum = 0;
+    let price = 0;
 
-    // Livro de ofertas
-    let book = await walks[walks.length - 1].exchange.getOrderBook(walks[walks.length - 1].symbol, 'ALL', 3);
-    // Verificando a ação 
-    let orders = walks[walks.length - 1].action === 'buy' ? book.sell : book.buy;
+    for (let [i, walk] of arb.walks.entries()) {      
+      // Livro de ofertas
+      let book = await walk.exchange.getOrderBook(walk.symbol, 'ALL', 3);
+      // Verificando a ação 
+      let orders = walk.action === 'buy' ? book.sell : book.buy;
 
-    if (walks[walks.length - 1].action === 'sell') {
-      for (let [i, order] of orders.entries()) {
-        sum = sum + order.Quantity;
-        // Montando o preço da rotina
-        if (walks[walks.length - 1].base === 'BRL' || walks[walks.length - 1].base === 'USDT' && (walks.length - 1) > 1) {
-          output = walks[walks.length - 1].quantity;
+      price = i === 0 ? arb.entry : arb.walks[i - 1].quantity;
+
+      for (let [s, order] of orders.entries()) {
+        if (order.Rate >= 1) {
+          sum = sum + (order.Quantity * order.Rate);
         } else {
-          output = this.calcQntSell(walks[walks.length - 1].quantity, order.Rate, walks[walks.length - 1].fee);
+          sum = sum + order.Quantity;
         }
-        // Analisando entre as ordens se existe algum lucro
-        if (output > arb.entry && walks[walks.length - 1].quantity <= sum) {
-          walks[walks.length - 1].price = order.Rate;
-          break;
-        }
-      }
-    } else {
-      for (let [index, order] of orders.entries()) {
-        sum = sum + order.Quantity;
-        // Montando o preço da rotina
-        if (walks[walks.length - 1].base === 'BRL' || walks[walks.length - 1].base === 'USDT' && (walks.length - 1) > 1) {
-          output = walks[walks.length - 1].quantity
+        
+        if (walk.action === 'sell') {
+          output = this.mask(this.calcQntSell(price, order.Rate, walk.fee), 8);
         } else {
-          output = this.calcQntBuy(walks[walks.length - 1].quantity, order.Rate, walks[walks.length - 1].fee);
+          output = this.mask(this.calcQntBuy(price, order.Rate, walk.fee), 8);
         }
-        // Analisando entre as ordens se existe algum lucro
-        if (output > arb.entry && walks[walks.length - 1].quantity <= sum) {
+
+        if (output > arb.entry && walks[walks.length - 1].quantity <= sum) {     
           walks[walks.length - 1].price = order.Rate;
           break;
         }
@@ -314,8 +158,7 @@ class Hades {
     }
 
     return output;
-  }  
-
+  }
   // Ação de compra
   async oppTakerBuy(walk, entry) {
     // Comprar
@@ -347,7 +190,16 @@ class Hades {
   }
   // Controlador de ações
   async routine(walk, arb, index) {
-    if (index === 0) {
+    // Se iniciar com a Bullgain
+    if (index === 0 && walk.exchangeto === 8) {
+      if (walk.action === 'sell') {
+        // Vender
+        await this.oppTakerSell(walk, arb.entry);
+      } else if (walk.action === 'buy') {
+        // Comprar
+        await this.oppTakerBuy(walk, arb.entry);                    
+      }  
+    } else if (index === 0) {
       // Enviando BTC ou USDT para outras exchanges
       await this.rebalancingBalance(walk, arb.entry);
 
@@ -357,7 +209,7 @@ class Hades {
       } else if (walk.action === 'buy') {
         // Comprar
         await this.oppTakerBuy(walk, arb.entry);                    
-      }  
+      } 
     } else {
       if (walk.action === 'sell') {
         // Vender
@@ -380,19 +232,16 @@ class Hades {
           const profit = await this.calcProfitOutput(arb);
 
           if (profit > arb.entry) {
-            /*for (let [y, walk] of walks.entries()) {
+            for (let [y, walk] of walks.entries()) {
               // Iniciando rotinas
               await this.routine(walk, arb, y);
             }
-            await Telegram.sendMessage(`[${arb.name}]: ${sprintf(`%.8f`, profit)}`);
-            console.log('Enviando notificação por telegram');*/
-            console.log(arb)
-            process.exit()
+            await Telegram.sendMessage(`[${arb.name}]: ${profit}`);
+            console.log('Enviando notificação por telegram'); 
           } else {
-            console.log(arb.name, sprintf(`%.8f`, profit));
+            console.log(arb.name, profit);
           }
-          await this.wait(1000);
-          
+          await this.wait(1000);          
         }  
       } catch(e) {
         console.log(e.message)
--- arbitration.js
+++ arbitration.js
@@ -5,8 +5,97 @@ const Bitrecife = require('./bitrecife.js');
 const Bleutrade = require('./bleutrade.js');
 const Bullgain = require('./bullgain.js');
 const Staging = require('./staging.js');
+const Bomesp = require('./bomesp.js');
 
 const Arbitration = [
+  {
+    name: 'BTC_USDT',
+    entry: 0.2,
+    walks: [
+      {
+        exchangeto: 2,
+        exchange: Exc,
+        fee: 0.9975,
+        price: 0,
+        quantity: 0,
+        action: 'buy',
+        symbol: 'BTC_USDT',
+        quote: 'BTC',
+        base: 'USDT',
+        receive: {
+          asset: 'USDT',
+          exchangeto: 2,
+          mail: 'tiago.a.trigo@gmail.com'
+        },
+        transfer: {
+          asset: 'BTC',
+          exchangeto: 1,
+          mail: 'tiago.a.trigo@gmail.com'
+        }
+      },
+      {
+        exchangeto: 1,
+        exchange: Bleutrade,
+        fee: 0.0015,
+        price: 0,
+        quantity: 0,
+        action: 'sell',
+        symbol: 'BTC_USDT',
+        quote: 'BTC',
+        base: 'USDT',
+        receive: null,
+        transfer: {
+          asset: 'BTC',
+          exchangeto: 9,
+          mail: 'tiago.a.trigo@gmail.com'
+        }
+      },
+    ]
+  },
+  {
+    name: 'BTC_USDT',
+    entry: 0.2,
+    walks: [
+      {
+        exchangeto: 1,
+        exchange: Bleutrade,
+        fee: 0.9985,
+        price: 0,
+        quantity: 0,
+        action: 'buy',
+        symbol: 'BTC_USDT',
+        quote: 'BTC',
+        base: 'USDT',
+        receive: {
+          asset: 'USDT',
+          exchangeto: 1,
+          mail: 'tiago.a.trigo@gmail.com'
+        },
+        transfer: {
+          asset: 'BTC',
+          exchangeto: 2,
+          mail: 'tiago.a.trigo@gmail.com'
+        }
+      },
+      {
+        exchangeto: 2,
+        exchange: Exc,
+        fee: 0.0025,
+        price: 0,
+        quantity: 0,
+        action: 'sell',
+        symbol: 'BTC_USDT',
+        quote: 'BTC',
+        base: 'USDT',
+        receive: null,
+        transfer: {
+          asset: 'USDT',
+          exchangeto: 9,
+          mail: 'tiago.a.trigo@gmail.com'
+        }
+      },
+    ]
+  },
   {
     name: 'ETH_BTC',
     entry: 0.0002,
@@ -315,165 +404,59 @@ const Arbitration = [
       },
     ]
   },
-  {
-    name: 'BTC_ETH_USDT_BTC',
-    entry: 0.0002,
-    walks: [
-      {
-        exchangeto: 1,
-        exchange: Bleutrade,
-        fee: 0.9985,
-        price: 0,
-        quantity: 0,
-        action: 'buy',
-        symbol: 'ETH_BTC',
-        quote: 'ETH',
-        base: 'BTC',
-        receive: {
-          asset: 'BTC',
-          exchangeto: 1,
-          mail: 'tiago.a.trigo@gmail.com'
-        },
-        transfer: null
-      },
-      {
-        exchangeto: 1,
-        exchange: Bleutrade,
-        fee: 0.0015,
-        price: 0,
-        quantity: 0,
-        action: 'sell',
-        symbol: 'ETH_USDT',
-        quote: 'ETH',
-        base: 'USDT',
-        receive: null,
-        transfer: null
-      },
-      {
-        exchangeto: 1,
-        exchange: Bleutrade,
-        fee: 0.9985,
-        price: 0,
-        quantity: 0,
-        action: 'buy',
-        symbol: 'BTC_USDT',
-        quote: 'BTC',
-        base: 'USDT',
-        receive: null,
-        transfer: {
-          asset: 'BTC',
-          exchangeto: 9,
-          mail: 'tiago.a.trigo@gmail.com'
-        }
-      },
-    ]
-  },
-  {
-    name: 'BTC_LTC_USDT_BTC',
-    entry: 0.0002,
-    walks: [
-      {
-        exchangeto: 1,
-        exchange: Bleutrade,
-        fee: 0.9985,
-        price: 0,
-        quantity: 0,
-        action: 'buy',
-        symbol: 'LTC_BTC',
-        quote: 'LTC',
-        base: 'BTC',
-        receive: {
-          asset: 'BTC',
-          exchangeto: 1,
-          mail: 'tiago.a.trigo@gmail.com'
-        },
-        transfer: null
-      },
-      {
-        exchangeto: 1,
-        exchange: Bleutrade,
-        fee: 0.0015,
-        price: 0,
-        quantity: 0,
-        action: 'sell',
-        symbol: 'LTC_USDT',
-        quote: 'LTC',
-        base: 'USDT',
-        receive: null,
-        transfer: null
-      },
-      {
-        exchangeto: 1,
-        exchange: Bleutrade,
-        fee: 0.9985,
-        price: 0,
-        quantity: 0,
-        action: 'buy',
-        symbol: 'BTC_USDT',
-        quote: 'BTC',
-        base: 'USDT',
-        receive: null,
-        transfer: {
-          asset: 'BTC',
-          exchangeto: 9,
-          mail: 'tiago.a.trigo@gmail.com'
-        }
-      },
-    ]
-  },
-  {
-    name: 'BTC_NBC_USDT_BTC',
-    entry: 0.0002,
-    walks: [
-      {
-        exchangeto: 1,
-        exchange: Bleutrade,
-        fee: 0.9985,
-        price: 0,
-        quantity: 0,
-        action: 'buy',
-        symbol: 'NBC_BTC',
-        quote: 'NBC',
-        base: 'BTC',
-        receive: {
-          asset: 'BTC',
-          exchangeto: 1,
-          mail: 'tiago.a.trigo@gmail.com'
-        },
-        transfer: null
-      },
-      {
-        exchangeto: 1,
-        exchange: Bleutrade,
-        fee: 0.0015,
-        price: 0,
-        quantity: 0,
-        action: 'sell',
-        symbol: 'NBC_USDT',
-        quote: 'NBC',
-        base: 'USDT',
-        receive: null,
-        transfer: null
-      },
-      {
-        exchangeto: 1,
-        exchange: Bleutrade,
-        fee: 0.9985,
-        price: 0,
-        quantity: 0,
-        action: 'buy',
-        symbol: 'BTC_USDT',
-        quote: 'BTC',
-        base: 'USDT',
-        receive: null,
-        transfer: {
-          asset: 'BTC',
-          exchangeto: 9,
-          mail: 'tiago.a.trigo@gmail.com'
-        }
-      },
-    ]
-  },
+  // {
+  //   name: 'BTC_USDT_DOGE_BTC',
+  //   entry: 0.0002,
+  //   walks: [
+  //     {
+  //       exchangeto: 2,
+  //       exchange: Exc,
+  //       fee: 0.0025,
+  //       price: 0,
+  //       quantity: 0,
+  //       action: 'sell',
+  //       symbol: 'BTC_USDT',
+  //       quote: 'BTC',
+  //       base: 'USDT',
+  //       receive: {
+  //         asset: 'BTC',
+  //         exchangeto: 2,
+  //         mail: 'tiago.a.trigo@gmail.com'
+  //       },
+  //       transfer: null
+  //     },
+  //     {
+  //       exchangeto: 2,
+  //       exchange: Exc,
+  //       fee: 0.9975,
+  //       price: 0,
+  //       quantity: 0,
+  //       action: 'buy',
+  //       symbol: 'DOGE_USDT',
+  //       quote: 'DOGE',
+  //       base: 'USDT',
+  //       receive: null,
+  //       transfer: null
+  //     },
+  //     {
+  //       exchangeto: 2,
+  //       exchange: Exc,
+  //       fee: 0.0025,
+  //       price: 0,
+  //       quantity: 0,
+  //       action: 'sell',
+  //       symbol: 'DOGE_BTC',
+  //       quote: 'DOGE',
+  //       base: 'BTC',
+  //       receive: null,
+  //       transfer: {
+  //         asset: 'BTC',
+  //         exchangeto: 9,
+  //         mail: 'tiago.a.trigo@gmail.com'
+  //       }
+  //     },
+  //   ]
+  // },
 ];
 
 module.exports = Arbitration;

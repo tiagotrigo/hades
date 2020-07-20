8e7321386a9e558cfd3d3c047d96c1ac4fd71254
tiagotrigo <tiagotrigo@bleu.com.br>
2020-07-20 19:43:58 -0300

    Nova versão para calcular a quantidade

--- app.js
+++ app.js
@@ -24,12 +24,26 @@ class Hades {
   }
 
   calcQntBuy(entry, price, fee) {
-    let quantity = (entry * fee) / price;
+    let tax = (1 - fee) * entry;
+    let quantity = tax / price;
+
     return quantity;
   }
 
   calcQntSell(entry, price, fee) {
-    let quantity = (entry * price) * (1 - fee);
+    let tax = entry * (1 - fee);
+    let quantity = tax / price;
+
+    return quantity;
+  }
+
+  calcBuyProfit(entry, price, fee) {
+    let quantity = entry / price;
+    return quantity;
+  }
+
+  calcSellProfit(entry, price, fee) {
+    let quantity = entry * price;
     return quantity;
   }
 
@@ -68,83 +82,69 @@ class Hades {
   }
 
   async calcQntOutput(arb) {
-    let sum = 0;
-    let qnt = 0;
-    let price = null;
+    let total = 0;
+    let value = 0;
+    let valueBook = 0;
+    let quantity = 0;
+    let price = 0;
+
 
     for (let [i, walk] of arb.walks.entries()) {
+
       // Livro de ofertas
       let book = await walk.exchange.getOrderBook(walk.symbol, 'ALL', 5);
       // Verificando a mascara
       let markets = await walk.exchange.getMarkets();
       let decimal = markets.find(i => walk.symbol === i.MarketName);
       // Verificando a ação 
-      let orders = walk.action === 'buy' ? book.sell : book.buy;
+      let orders = walk.action === 'buy' ? book.sell : book.buy;     
+      // Reset
+      walk.price = 0;
+      walk.quantity = 0;
+      walk.total = 0;
 
-      if (i === 0) {
-        walk.quantity = arb.entry;
-        
-        price = orders.find(i => {
-          if (i.Rate >= 1) {
-            sum = sum + (i.Rate * i.Quantity);
+      value = i === 0 ? arb.entry : arb.walks[i - 1].quantity;
+
+      if (walk.action === 'buy') {       
+        for (let x = 0; x < orders.length; x++) {
+          valueBook = orders[x].Quantity * orders[x].Rate;          
+          if (valueBook < value) {
+            value = value - valueBook;
+            walk.price = orders[x].Rate;
+            walk.quantity = walk.quantity + orders[x].Quantity;
           } else {
-            sum = sum + i.Quantity;
+            walk.quantity = walk.quantity + (value / orders[x].Rate);
+            walk.price = orders[x].Rate;
+            walk.total = walk.quantity * walk.fee;
+            break;
           }
-          return walk.quantity <= sum;
-        });
-        
-        walk.price = price.Rate;
-        
-        if (walk.action === 'sell') {
-          walk.quantity = this.mask(this.calcQntSell(walk.quantity, walk.price, walk.fee), decimal.DividendDecimal);
-        } else {
-          walk.quantity = this.mask(this.calcQntBuy(walk.quantity, walk.price, walk.fee), decimal.DividendDecimal);
         }
-      } else if (i > 0 && walk.quote != 'USDT' || walk.quote != 'BTC' || walk.quote != 'BRL') {
-        walk.quantity = arb.walks[i - 1].quantity;
-
-        price = orders.find(i => {
-          if (i.Rate >= 1) {
-            sum = sum + (i.Rate * i.Quantity);
+      } else {
+        for (let x = 0; x < orders.length; i++) {
+          if (orders[x].Quantity < value) {
+            value = value - orders[x].Quantity;
+            walk.quantity = walk.quantity + orders[x].Quantity;
+            walk.price = orders[x].Rate;
+            walk.total = walk.total + (orders[x].Quantity * orders[x].Rate) * walk.fee; 
           } else {
-            sum = sum + i.Quantity;
+            walk.quantity = walk.quantity + value;
+            walk.price = orders[x].Rate;
+            walk.total = walk.total + (value * orders[x].Rate) * walk.fee;
+            break;
           }
-          return walk.quantity <= sum;
-        });
-        
-        walk.price = price.Rate;
+        }
       }
     }
   }  
   // Segundo passo
-  async calcProfitOutput(arb) {    
-    let output = 0;
-
-    if (arb.walks[arb.walks.length - 1].action === 'sell') {
-      output = this.calcQntSell(arb.walks[arb.walks.length - 1].quantity, arb.walks[arb.walks.length - 1].price, arb.walks[arb.walks.length - 1].fee);
-    } else {
-      output = this.calcQntBuy(arb.walks[arb.walks.length - 1].quantity, arb.walks[arb.walks.length - 1].price, arb.walks[arb.walks.length - 1].fee);
-    } 
-    
-    return this.mask(output, 8);
+  async calcProfitOutput(arb) {        
+    return arb.entry - arb.walks[arb.walks.length - 1].quantity;
   }
   // Ação de compra
   async oppTakerBuy(walk, entry, index) {
-    let trade = await walk.exchange.getBalance(walk.trade);
-
-    if (index === 0 && (walk.quote === 'USDT' || walk.quote === 'BTC' || walk.quote === 'BRL')) {    
-      // Comprar
-      await walk.exchange.setBuyMarket(walk.symbol, entry);
-      console.log(`Troca de ${walk.base} por ${walk.quote} (${entry})`);
-    } else if (index === 0 && (walk.quote != 'USDT' || walk.quote != 'BTC' || walk.quote != 'BRL')) {
-      // Comprar
-      await walk.exchange.setBuyMarket(walk.symbol, walk.quantity);
-      console.log(`Troca de ${walk.base} por ${walk.quote} (${walk.quantity})`);
-    } else {
-      // Comprar
-      await walk.exchange.setBuyMarket(walk.symbol, trade.data.result[0].Available);
-      console.log(`Troca de ${walk.base} por ${walk.quote} (${trade.data.result[0].Available})`);
-    }
+    // Comprar
+    await walk.exchange.setBuyLimit(walk.symbol, walk.price, this.mask(walk.quantity, 8));
+    console.log(`Troca de ${walk.base} por ${walk.quote} (${walk.quantity})`);
     // É preciso transferir ?
     if (walk.transfer) {
       let wallet = await walk.exchange.getBalance(walk.transfer.asset);
@@ -156,21 +156,9 @@ class Hades {
   }
   // Ação de venda
   async oppTakerSell(walk, entry, index) { 
-    let trade = await walk.exchange.getBalance(walk.trade);
-
-    if (index === 0 && (walk.quote === 'USDT' || walk.quote === 'BTC' || walk.quote === 'BRL')) {    
-      // Vender
-      await walk.exchange.setSellMarket(walk.symbol, entry);
-      console.log(`Troca de ${walk.base} por ${walk.quote} (${entry})`);
-    } else if (index === 0 && (walk.quote != 'USDT' || walk.quote != 'BTC' || walk.quote != 'BRL')) {
-      // Vender
-      await walk.exchange.setSellMarket(walk.symbol, walk.quantity);
-      console.log(`Troca de ${walk.base} por ${walk.quote} (${walk.quantity})`);
-    } else {
-      // Vender
-      await walk.exchange.setSellMarket(walk.symbol, trade.data.result[0].Available);
-      console.log(`Troca de ${walk.base} por ${walk.quote} (${trade.data.result[0].Available})`);
-    }
+    // Vender
+    await walk.exchange.setSellLimit(walk.symbol, walk.price, this.mask(walk.quantity, 8));
+    console.log(`Troca de ${walk.base} por ${walk.quote} (${walk.quantity})`);
     
     // É preciso transferir ?
     if (walk.transfer) {
@@ -184,7 +172,7 @@ class Hades {
   // Controlador de ações
   async routine(walk, arb, index) {
     // Se iniciar com a Bullgain
-    if (index === 0 && walk.exchangeto === 8) {
+    if (index === 0 && walk.exchangeto === 9) {
       if (walk.action === 'sell') {
         // Vender
         await this.oppTakerSell(walk, arb.entry, index);
@@ -225,12 +213,13 @@ class Hades {
           const profit = await this.calcProfitOutput(arb);
 
           if (profit > arb.entry) {
-            for (let [y, walk] of walks.entries()) {
-              // Iniciando rotinas
-              await this.routine(walk, arb, y);
-            }
-            await Telegram.sendMessage(`[${arb.name}]: ${profit}`);
-            console.log('Enviando notificação para @tiagotrigo');
+            // for (let [y, walk] of walks.entries()) {
+            //   // Iniciando rotinas
+            //   await this.routine(walk, arb, y);
+            // }
+            // await Telegram.sendMessage(`[${arb.name}]: ${profit}`);
+            // console.log('Enviando notificação para @tiagotrigo');            
+            console.log(arb)
           } else {
             console.log(arb.name, profit);
           }
--- arbitration.js
+++ arbitration.js
@@ -9,6 +9,98 @@ const Bomesp = require('./bomesp.js');
 
 const Arbitration = [
   {
+    name: 'BTC_USDT',
+    entry: 0.2,
+    walks: [
+      {
+        exchangeto: 2,
+        exchange: Exc,
+        fee: 0.9975,
+        price: 0,
+        quantity: 0,
+        action: 'buy',
+        symbol: 'BTC_USDT',
+        quote: 'BTC',
+        base: 'USDT',
+        total: 0,
+        receive: {
+          asset: 'USDT',
+          exchangeto: 2,
+          mail: 'tiago.a.trigo@gmail.com'
+        },
+        transfer: {
+          asset: 'BTC',
+          exchangeto: 1,
+          mail: 'tiago.a.trigo@gmail.com'
+        }
+      },
+      {
+        exchangeto: 1,
+        exchange: Bleutrade,
+        fee: 0.9985,
+        price: 0,
+        quantity: 0,
+        action: 'sell',
+        symbol: 'BTC_USDT',
+        quote: 'BTC',
+        base: 'USDT',
+        total: 0,
+        receive: null,
+        transfer: {
+          asset: 'USDT',
+          exchangeto: 9,
+          mail: 'tiago.a.trigo@gmail.com'
+        }
+      },
+    ]
+  },
+  {
+    name: 'BTC_USDT',
+    entry: 0.2,
+    walks: [
+      {
+        exchangeto: 1,
+        exchange: Bleutrade,
+        fee: 0.9985,
+        price: 0,
+        quantity: 0,
+        action: 'buy',
+        symbol: 'BTC_USDT',
+        quote: 'BTC',
+        base: 'USDT',
+        total: 0,
+        receive: {
+          asset: 'USDT',
+          exchangeto: 1,
+          mail: 'tiago.a.trigo@gmail.com'
+        },
+        transfer: {
+          asset: 'BTC',
+          exchangeto: 2,
+          mail: 'tiago.a.trigo@gmail.com'
+        }
+      },
+      {
+        exchangeto: 2,
+        exchange: Exc,
+        fee: 0.9975,
+        price: 0,
+        quantity: 0,
+        action: 'sell',
+        symbol: 'BTC_USDT',
+        quote: 'BTC',
+        base: 'USDT',
+        total: 0,
+        receive: null,
+        transfer: {
+          asset: 'USDT',
+          exchangeto: 9,
+          mail: 'tiago.a.trigo@gmail.com'
+        }
+      },
+    ]
+  },
+  {
     name: 'ETH_USDT',
     entry: 0.2,
     walks: [
@@ -22,7 +114,7 @@ const Arbitration = [
         symbol: 'ETH_USDT',
         quote: 'ETH',
         base: 'USDT',
-        trade: 'ETH',
+        total: 0,
         receive: {
           asset: 'USDT',
           exchangeto: 2,
@@ -37,14 +129,14 @@ const Arbitration = [
       {
         exchangeto: 1,
         exchange: Bleutrade,
-        fee: 0.0015,
+        fee: 0.9985,
         price: 0,
         quantity: 0,
         action: 'sell',
         symbol: 'ETH_USDT',
         quote: 'ETH',
         base: 'USDT',
-        trade: 'ETH',
+        total: 0,
         receive: null,
         transfer: {
           asset: 'USDT',
@@ -54,6 +146,375 @@ const Arbitration = [
       },
     ]
   },
+  {
+    name: 'ETH_USDT',
+    entry: 0.2,
+    walks: [
+      {
+        exchangeto: 1,
+        exchange: Bleutrade,
+        fee: 0.9985,
+        price: 0,
+        quantity: 0,
+        action: 'buy',
+        symbol: 'ETH_USDT',
+        quote: 'ETH',
+        base: 'USDT',
+        total: 0,
+        receive: {
+          asset: 'USDT',
+          exchangeto: 1,
+          mail: 'tiago.a.trigo@gmail.com'
+        },
+        transfer: {
+          asset: 'ETH',
+          exchangeto: 2,
+          mail: 'tiago.a.trigo@gmail.com'
+        }
+      },
+      {
+        exchangeto: 2,
+        exchange: Exc,
+        fee: 0.9975,
+        price: 0,
+        quantity: 0,
+        action: 'sell',
+        symbol: 'ETH_USDT',
+        quote: 'ETH',
+        base: 'USDT',
+        total: 0,
+        receive: null,
+        transfer: {
+          asset: 'USDT',
+          exchangeto: 9,
+          mail: 'tiago.a.trigo@gmail.com'
+        }
+      },
+    ]
+  },
+  {
+    name: 'LTC_USDT',
+    entry: 0.2,
+    walks: [
+      {
+        exchangeto: 2,
+        exchange: Exc,
+        fee: 0.9975,
+        price: 0,
+        quantity: 0,
+        action: 'buy',
+        symbol: 'LTC_USDT',
+        quote: 'LTC',
+        base: 'USDT',
+        total: 0,
+        receive: {
+          asset: 'USDT',
+          exchangeto: 2,
+          mail: 'tiago.a.trigo@gmail.com'
+        },
+        transfer: {
+          asset: 'LTC',
+          exchangeto: 1,
+          mail: 'tiago.a.trigo@gmail.com'
+        }
+      },
+      {
+        exchangeto: 1,
+        exchange: Bleutrade,
+        fee: 0.9985,
+        price: 0,
+        quantity: 0,
+        action: 'sell',
+        symbol: 'LTC_USDT',
+        quote: 'LTC',
+        base: 'USDT',
+        total: 0,
+        receive: null,
+        transfer: {
+          asset: 'USDT',
+          exchangeto: 9,
+          mail: 'tiago.a.trigo@gmail.com'
+        }
+      },
+    ]
+  },
+  {
+    name: 'LTC_USDT',
+    entry: 0.2,
+    walks: [
+      {
+        exchangeto: 1,
+        exchange: Bleutrade,
+        fee: 0.9985,
+        price: 0,
+        quantity: 0,
+        action: 'buy',
+        symbol: 'LTC_USDT',
+        quote: 'LTC',
+        base: 'USDT',
+        total: 0,
+        receive: {
+          asset: 'USDT',
+          exchangeto: 1,
+          mail: 'tiago.a.trigo@gmail.com'
+        },
+        transfer: {
+          asset: 'LTC',
+          exchangeto: 2,
+          mail: 'tiago.a.trigo@gmail.com'
+        }
+      },
+      {
+        exchangeto: 2,
+        exchange: Exc,
+        fee: 0.9975,
+        price: 0,
+        quantity: 0,
+        action: 'sell',
+        symbol: 'LTC_USDT',
+        quote: 'LTC',
+        base: 'USDT',
+        total: 0,
+        receive: null,
+        transfer: {
+          asset: 'USDT',
+          exchangeto: 9,
+          mail: 'tiago.a.trigo@gmail.com'
+        }
+      },
+    ]
+  },
+  {
+    name: 'ETH_BTC',
+    entry: 0.0002,
+    walks: [
+      {
+        exchangeto: 2,
+        exchange: Exc,
+        fee: 0.9975,
+        price: 0,
+        quantity: 0,
+        action: 'buy',
+        symbol: 'ETH_BTC',
+        quote: 'ETH',
+        base: 'BTC',
+        total: 0,
+        receive: {
+          asset: 'BTC',
+          exchangeto: 2,
+          mail: 'tiago.a.trigo@gmail.com'
+        },
+        transfer: {
+          asset: 'ETH',
+          exchangeto: 1,
+          mail: 'tiago.a.trigo@gmail.com'
+        }
+      },
+      {
+        exchangeto: 1,
+        exchange: Bleutrade,
+        fee: 0.9985,
+        price: 0,
+        quantity: 0,
+        action: 'sell',
+        symbol: 'ETH_BTC',
+        quote: 'ETH',
+        base: 'BTC',
+        total: 0,
+        receive: null,
+        transfer: {
+          asset: 'BTC',
+          exchangeto: 9,
+          mail: 'tiago.a.trigo@gmail.com'
+        }
+      },
+    ]
+  },
+  {
+    name: 'ETH_BTC',
+    entry: 0.0002,
+    walks: [
+      {
+        exchangeto: 1,
+        exchange: Bleutrade,
+        fee: 0.9985,
+        price: 0,
+        quantity: 0,
+        action: 'buy',
+        symbol: 'ETH_BTC',
+        quote: 'ETH',
+        base: 'BTC',
+        total: 0,
+        receive: {
+          asset: 'BTC',
+          exchangeto: 1,
+          mail: 'tiago.a.trigo@gmail.com'
+        },
+        transfer: {
+          asset: 'ETH',
+          exchangeto: 2,
+          mail: 'tiago.a.trigo@gmail.com'
+        }
+      },
+      {
+        exchangeto: 2,
+        exchange: Exc,
+        fee: 0.9975,
+        price: 0,
+        quantity: 0,
+        action: 'sell',
+        symbol: 'ETH_BTC',
+        quote: 'ETH',
+        base: 'BTC',
+        total: 0,
+        receive: null,
+        transfer: {
+          asset: 'BTC',
+          exchangeto: 9,
+          mail: 'tiago.a.trigo@gmail.com'
+        }
+      },
+    ]
+  },
+  {
+    name: 'LTC_BTC',
+    entry: 0.0002,
+    walks: [
+      {
+        exchangeto: 2,
+        exchange: Exc,
+        fee: 0.9975,
+        price: 0,
+        quantity: 0,
+        action: 'buy',
+        symbol: 'LTC_BTC',
+        quote: 'LTC',
+        base: 'BTC',
+        total: 0,
+        receive: {
+          asset: 'BTC',
+          exchangeto: 2,
+          mail: 'tiago.a.trigo@gmail.com'
+        },
+        transfer: {
+          asset: 'LTC',
+          exchangeto: 1,
+          mail: 'tiago.a.trigo@gmail.com'
+        }
+      },
+      {
+        exchangeto: 1,
+        exchange: Bleutrade,
+        fee: 0.9985,
+        price: 0,
+        quantity: 0,
+        action: 'sell',
+        symbol: 'LTC_BTC',
+        quote: 'LTC',
+        base: 'BTC',
+        total: 0,
+        receive: null,
+        transfer: {
+          asset: 'BTC',
+          exchangeto: 9,
+          mail: 'tiago.a.trigo@gmail.com'
+        }
+      },
+    ]
+  },
+  {
+    name: 'LTC_BTC',
+    entry: 0.0002,
+    walks: [
+      {
+        exchangeto: 1,
+        exchange: Bleutrade,
+        fee: 0.9985,
+        price: 0,
+        quantity: 0,
+        action: 'buy',
+        symbol: 'LTC_BTC',
+        quote: 'LTC',
+        base: 'BTC',
+        total: 0,
+        receive: {
+          asset: 'BTC',
+          exchangeto: 1,
+          mail: 'tiago.a.trigo@gmail.com'
+        },
+        transfer: {
+          asset: 'LTC',
+          exchangeto: 2,
+          mail: 'tiago.a.trigo@gmail.com'
+        }
+      },
+      {
+        exchangeto: 2,
+        exchange: Exc,
+        fee: 0.9975,
+        price: 0,
+        quantity: 0,
+        action: 'sell',
+        symbol: 'LTC_BTC',
+        quote: 'LTC',
+        base: 'BTC',
+        total: 0,
+        receive: null,
+        transfer: {
+          asset: 'BTC',
+          exchangeto: 9,
+          mail: 'tiago.a.trigo@gmail.com'
+        }
+      },
+    ]
+  },
+  // 3 Passos
+  // {
+  //   name: 'DOGE_BTC_USDT_BTC',
+  //   entry: 0.0002,
+  //   walks: [
+  //     {
+  //       exchangeto: 2,
+  //       exchange: Exc,
+  //       fee: 0.0025,
+  //       price: 0,
+  //       quantity: 0,
+  //       action: 'buy',
+  //       symbol: 'DOGE_BTC',
+  //       quote: 'DOGE',
+  //       base: 'BTC',
+  //       trade: 'DOGE',
+  //       receive: {
+  //         asset: 'BTC',
+  //         exchangeto: 2,
+  //         mail: 'tiago.a.trigo@gmail.com'
+  //       },
+  //       transfer: {
+  //         asset: 'DOGE',
+  //         exchangeto: 2,
+  //         mail: 'tiago.a.trigo@gmail.com'
+  //       }
+  //     },
+  //     {
+  //       exchangeto: 2,
+  //       exchange: Exc,
+  //       fee: 0.0025,
+  //       price: 0,
+  //       quantity: 0,
+  //       action: 'sell',
+  //       symbol: 'LTC_BTC',
+  //       quote: 'LTC',
+  //       base: 'BTC',
+  //       trade: 'LTC',
+  //       receive: null,
+  //       transfer: {
+  //         asset: 'BTC',
+  //         exchangeto: 9,
+  //         mail: 'tiago.a.trigo@gmail.com'
+  //       }
+  //     },
+  //   ]
+  // },
 ];
 
 module.exports = Arbitration;

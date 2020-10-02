11be1ae794910f342a71949ac052ef01f1b32c2f
tiagotrigo <tiagotrigo@bleu.com.br>
2020-10-02 14:39:11 -0300

    Ordem em USDC

--- arbitration.js
+++ arbitration.js
@@ -71,6 +71,66 @@ let Arbitration = [
       },
     ]
   },
+  {
+    name: 'BTC_BRL >> BTC_USDC >> USDC_BRL',
+    entry: 500,
+    decimal: 2,
+    walks: [
+      {
+        exchangeto: 3,
+        exchange: Bitrecife,
+        fee: 0.9976,
+        price: 0,
+        quantity: 0,
+        action: 'buy',
+        symbol: 'BTC_BRL',
+        quote: 'BTC',
+        base: 'BRL',
+        total: 0,
+        trade: 'BRL',
+        receive: null,
+        transfer: {
+          asset: 'BTC',
+          exchangeto: 1,
+          mail: 'tiago.a.trigo@gmail.com'
+        }
+      },
+      {
+        exchangeto: 1,
+        exchange: Bleutrade,
+        fee: 0.9985,
+        price: 0,
+        quantity: 0,
+        action: 'sell',
+        symbol: 'BTC_USDC',
+        quote: 'BTC',
+        base: 'USDC',
+        total: 0,
+        trade: 'BTC',
+        receive: null,
+        transfer: {
+          asset: 'USDC',
+          exchangeto: 3,
+          mail: 'tiago.a.trigo@gmail.com'
+        }
+      },
+      {
+        exchangeto: 3,
+        exchange: Bitrecife,
+        fee: 0.9976,
+        price: 0,
+        quantity: 0,
+        action: 'sell',
+        symbol: 'USDC_BRL',
+        quote: 'USDC',
+        base: 'BRL',
+        total: 0,
+        trade: 'USDC',
+        receive: null,
+        transfer: null
+      },
+    ]
+  },
   // USDT
   {
     name: 'USDT_BRL >> BTC_BRL >> BTC_USDT',

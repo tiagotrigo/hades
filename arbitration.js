473f677f1d08092000480846f1c362fe59e6b7ba
tiagotrigo <tiagotrigo@bleu.com.br>
2020-10-28 10:10:14 -0300

    Entradas de USDT em LTC e ETH

--- arbitration.js
+++ arbitration.js
@@ -130,130 +130,104 @@ let Arbitration = [
     ]
   },
   // USDT
-  // {
-  //   name: 'USDT_BRL >> BTC_BRL >> BTC_USDT',
-  //   entry: 8,
-  //   decimal: 6,
-  //   walks: [
-  //     {
-  //       exchangeto: 3,
-  //       exchange: Bitrecife,
-  //       fee: 0.9976,
-  //       price: 0,
-  //       quantity: 0,
-  //       action: 'sell',
-  //       symbol: 'USDT_BRL',
-  //       quote: 'USDT',
-  //       base: 'BRL',
-  //       total: 0,
-  //       trade: 'USDT',
-  //       receive: null,
-  //       transfer: null
-  //     },
-  //     {
-  //       exchangeto: 3,
-  //       exchange: Bitrecife,
-  //       fee: 0.9976,
-  //       price: 0,
-  //       quantity: 0,
-  //       action: 'buy',
-  //       symbol: 'BTC_BRL',
-  //       quote: 'BTC',
-  //       base: 'BRL',
-  //       total: 0,
-  //       trade: 'BRL',
-  //       receive: null,
-  //       transfer: {
-  //         asset: 'BTC',
-  //         exchangeto: 1,
-  //         mail: 'tiago.a.trigo@gmail.com'
-  //       }
-  //     },
-  //     {
-  //       exchangeto: 1,
-  //       exchange: Bleutrade,
-  //       fee: 0.9985,
-  //       price: 0,
-  //       quantity: 0,
-  //       action: 'sell',
-  //       symbol: 'BTC_USDT',
-  //       quote: 'BTC',
-  //       base: 'USDT',
-  //       total: 0,
-  //       trade: 'BTC',
-  //       receive: null,
-  //       transfer: {
-  //         asset: 'USDT',
-  //         exchangeto: 3,
-  //         mail: 'tiago.a.trigo@gmail.com'
-  //       }
-  //     },
-  //   ]
-  // },
-  // {
-  //   name: 'USDT_BRL >> BTC_BRL >> BTC_USDT',
-  //   entry: 8,
-  //   decimal: 6,
-  //   walks: [
-  //     {
-  //       exchangeto: 9,
-  //       exchange: Bullgain,
-  //       fee: 0.99684,
-  //       price: 0,
-  //       quantity: 0,
-  //       action: 'sell',
-  //       symbol: 'USDT_BRL',
-  //       quote: 'USDT',
-  //       base: 'BRL',
-  //       total: 0,
-  //       trade: 'USDT',
-  //       receive: {
-  //         asset: 'USDT',
-  //         exchangeto: 9,
-  //         mail: 'tiago.a.trigo@gmail.com'
-  //       },
-  //       transfer: null
-  //     },
-  //     {
-  //       exchangeto: 9,
-  //       exchange: Bullgain,
-  //       fee: 0.99684,
-  //       price: 0,
-  //       quantity: 0,
-  //       action: 'buy',
-  //       symbol: 'BTC_BRL',
-  //       quote: 'BTC',
-  //       base: 'BRL',
-  //       total: 0,
-  //       trade: 'BRL',
-  //       receive: null,
-  //       transfer: {
-  //         asset: 'BTC',
-  //         exchangeto: 1,
-  //         mail: 'tiago.a.trigo@gmail.com'
-  //       }
-  //     },
-  //     {
-  //       exchangeto: 1,
-  //       exchange: Bleutrade,
-  //       fee: 0.9985,
-  //       price: 0,
-  //       quantity: 0,
-  //       action: 'sell',
-  //       symbol: 'BTC_USDT',
-  //       quote: 'BTC',
-  //       base: 'USDT',
-  //       total: 0,
-  //       trade: 'BTC',
-  //       receive: null,
-  //       transfer: {
-  //         asset: 'USDT',
-  //         exchangeto: 3,
-  //         mail: 'tiago.a.trigo@gmail.com'
-  //       }
-  //     },
-  //   ]
-  // },
+  {
+    name: 'LTC_USDT >> LTC_USDT',
+    entry: 3,
+    decimal: 6,
+    walks: [
+      {
+        exchangeto: 1,
+        exchange: Bleutrade,
+        fee: 0.9985,
+        price: 0,
+        quantity: 0,
+        action: 'buy',
+        symbol: 'LTC_USDT',
+        quote: 'LTC',
+        base: 'USDT',
+        total: 0,
+        trade: 'USDT',
+        receive: {
+          asset: 'USDT',
+          exchangeto: 1,
+          mail: 'tiago.a.trigo@gmail.com'
+        },
+        transfer: {
+          asset: 'LTC',
+          exchangeto: 2,
+          mail: 'tiago.a.trigo@gmail.com'
+        }
+      },
+      {
+        exchangeto: 2,
+        exchange: Exc,
+        fee: 0.9975,
+        price: 0,
+        quantity: 0,
+        action: 'sell',
+        symbol: 'LTC_USDT',
+        quote: 'LTC',
+        base: 'USDT',
+        total: 0,
+        trade: 'LTC',
+        receive: null,
+        transfer: {
+          asset: 'USDT',
+          exchangeto: 3,
+          mail: 'tiago.a.trigo@gmail.com'
+        }
+      }
+    ]
+  },
+  {
+    name: 'ETH_USDT >> ETH_USDT',
+    entry: 3,
+    decimal: 6,
+    walks: [
+      {
+        exchangeto: 2,
+        exchange: Exc,
+        fee: 0.9975,
+        price: 0,
+        quantity: 0,
+        action: 'buy',
+        symbol: 'ETH_USDT',
+        quote: 'ETH',
+        base: 'USDT',
+        total: 0,
+        trade: 'USDT',
+        receive: {
+          asset: 'USDT',
+          exchangeto: 2,
+          mail: 'tiago.a.trigo@gmail.com'
+        },
+        transfer: {
+          asset: 'ETH',
+          exchangeto: 1,
+          mail: 'tiago.a.trigo@gmail.com'
+        }
+      },
+      {
+        exchangeto: 1,
+        exchange: Bleutrade,
+        fee: 0.9985,
+        price: 0,
+        quantity: 0,
+        action: 'sell',
+        symbol: 'ETH_USDT',
+        quote: 'ETH',
+        base: 'USDT',
+        total: 0,
+        trade: 'ETH',
+        receive: null,
+        transfer: {
+          asset: 'USDT',
+          exchangeto: 3,
+          mail: 'tiago.a.trigo@gmail.com'
+        }
+      }
+    ]
+  },
   // FIM USDT
 
   // CBRL

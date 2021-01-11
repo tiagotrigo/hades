10d9549243435e2fe5307b3bc9026be90204d5ad
tiagotrigo <tiagotrigo@bleu.com.br>
2021-01-11 12:11:27 -0300

    Atualizando para REAL

--- arbitration.js
+++ arbitration.js
@@ -10,7 +10,82 @@ const ComprarBitcoin = require('./comprarBitcoin.js');
 
 let Arbitration = [
   // BRL
-  /*{
+  {
+    name: 'ETH_BRL >> ETH_BRL >> USDT_BRL >> USDT_BRL',
+    entry: 200,
+    decimal: 2,
+    walks: [
+      {
+        exchangeto: 3,
+        exchange: Bitrecife,
+        fee: 0.9976,
+        price: 0,
+        quantity: 0,
+        action: 'buy',
+        symbol: 'ETH_BRL',
+        quote: 'ETH',
+        base: 'BRL',
+        total: 0,
+        trade: 'BRL',
+        receive: null,
+        transfer: {
+          asset: 'ETH',
+          exchangeto: 9,
+          mail: 'tiago.a.trigo@gmail.com'
+        }
+      },
+      {
+        exchangeto: 9,
+        exchange: Bullgain,
+        fee: 0.99685,
+        price: 0,
+        quantity: 0,
+        action: 'sell',
+        symbol: 'ETH_BRL',
+        quote: 'ETH',
+        base: 'BRL',
+        total: 0,
+        trade: 'ETH',
+        receive: null,
+        transfer: null
+      },
+      {
+        exchangeto: 9,
+        exchange: Bullgain,
+        fee: 0.99685,
+        price: 0,
+        quantity: 0,
+        action: 'buy',
+        symbol: 'USDT_BRL',
+        quote: 'USDT',
+        base: 'BRL',
+        total: 0,
+        trade: 'BRL',
+        receive: null,
+        transfer: {
+          asset: 'USDT',
+          exchangeto: 3,
+          mail: 'tiago.a.trigo@gmail.com'
+        }
+      },
+      {
+        exchangeto: 3,
+        exchange: Bitrecife,
+        fee: 0.9976,
+        price: 0,
+        quantity: 0,
+        action: 'sell',
+        symbol: 'USDT_BRL',
+        quote: 'USDT',
+        base: 'BRL',
+        total: 0,
+        trade: 'USDT',
+        receive: null,
+        transfer: null
+      }
+    ]
+  },
+  {
     name: 'USDT_BRL >> USDT_BRL >> BTC_BRL >> BTC_BRL',
     entry: 100,
     decimal: 2,
@@ -204,111 +279,10 @@ let Arbitration = [
         transfer: null
       },
     ]
-  },*/
-
-  // CBRL
-  /*{
-    name: 'USDT_CBRL >> USDT_CBRL',
-    entry: 100,
-    decimal: 2,
-    walks: [
-      {
-        exchangeto: 2,
-        exchange: Exc,
-        fee: 0.9975,
-        price: 0,
-        quantity: 0,
-        action: 'buy',
-        symbol: 'USDT_CBRL',
-        quote: 'USDT',
-        base: 'CBRL',
-        total: 0,
-        trade: 'CBRL',
-        receive: {
-          asset: 'CBRL',
-          exchangeto: 2,
-          mail: 'tiago.a.trigo@gmail.com'
-        },
-        transfer: {
-          asset: 'USDT',
-          exchangeto: 1,
-          mail: 'tiago.a.trigo@gmail.com'
-        }
-      },
-      {
-        exchangeto: 1,
-        exchange: Bleutrade,
-        fee: 0.9985,
-        price: 0,
-        quantity: 0,
-        action: 'sell',
-        symbol: 'USDT_CBRL',
-        quote: 'USDT',
-        base: 'CBRL',
-        total: 0,
-        trade: 'USDT',
-        receive: null,
-        transfer: {
-          asset: 'CBRL',
-          exchangeto: 3,
-          mail: 'tiago.a.trigo@gmail.com'
-        }
-      },
-    ]
-  },
-  {
-    name: 'USDT_CBRL >> USDT_CBRL',
-    entry: 100,
-    decimal: 2,
-    walks: [
-      {
-        exchangeto: 1,
-        exchange: Bleutrade,
-        fee: 0.9985,
-        price: 0,
-        quantity: 0,
-        action: 'buy',
-        symbol: 'USDT_CBRL',
-        quote: 'USDT',
-        base: 'CBRL',
-        total: 0,
-        trade: 'CBRL',
-        receive: {
-          asset: 'CBRL',
-          exchangeto: 1,
-          mail: 'tiago.a.trigo@gmail.com'
-        },
-        transfer: {
-          asset: 'USDT',
-          exchangeto: 2,
-          mail: 'tiago.a.trigo@gmail.com'
-        }
-      },
-      {
-        exchangeto: 2,
-        exchange: Exc,
-        fee: 0.9975,
-        price: 0,
-        quantity: 0,
-        action: 'sell',
-        symbol: 'USDT_CBRL',
-        quote: 'USDT',
-        base: 'CBRL',
-        total: 0,
-        trade: 'USDT',
-        receive: null,
-        transfer: {
-          asset: 'CBRL',
-          exchangeto: 3,
-          mail: 'tiago.a.trigo@gmail.com'
-        }
-      },
-    ]
-  },*/
-  // FIM CBRL
+  }
 
   // BTC  
-  {
+  /*{
     name: 'LTC_BTC >> LTC_BTC',
     entry: 0.001,
     decimal: 8,
@@ -938,7 +912,7 @@ let Arbitration = [
         }
       },
     ]
-  },
+  },*/
 ];
 
 module.exports = Arbitration;

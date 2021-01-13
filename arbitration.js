c017d2c071c6241e216a61c60e1c644c857a8daa
tiagotrigo <tiagotrigo@bleu.com.br>
2021-01-13 07:39:56 -0300

    Rota de USDT_BRL

--- arbitration.js
+++ arbitration.js
@@ -160,6 +160,81 @@ let Arbitration = [
       }
     ]
   },
+  {
+    name: 'USDT_BRL >> USDT_BRL >> ETH_BRL >> ETH_BRL',
+    entry: 200,
+    decimal: 2,
+    walks: [
+      {
+        exchangeto: 3,
+        exchange: Bitrecife,
+        fee: 0.9976,
+        price: 0,
+        quantity: 0,
+        action: 'buy',
+        symbol: 'USDT_BRL',
+        quote: 'USDT',
+        base: 'BRL',
+        total: 0,
+        trade: 'BRL',
+        receive: null,
+        transfer: {
+          asset: 'USDT',
+          exchangeto: 9,
+          mail: 'tiago.a.trigo@gmail.com'
+        }
+      },
+      {
+        exchangeto: 9,
+        exchange: Bullgain,
+        fee: 0.99685,
+        price: 0,
+        quantity: 0,
+        action: 'sell',
+        symbol: 'USDT_BRL',
+        quote: 'USDT',
+        base: 'BRL',
+        total: 0,
+        trade: 'USDT',
+        receive: null,
+        transfer: null
+      },
+      {
+        exchangeto: 9,
+        exchange: Bullgain,
+        fee: 0.99685,
+        price: 0,
+        quantity: 0,
+        action: 'buy',
+        symbol: 'ETH_BRL',
+        quote: 'ETH',
+        base: 'BRL',
+        total: 0,
+        trade: 'BRL',
+        receive: null,
+        transfer: {
+          asset: 'ETH',
+          exchangeto: 3,
+          mail: 'tiago.a.trigo@gmail.com'
+        }
+      },
+      {
+        exchangeto: 3,
+        exchange: Bitrecife,
+        fee: 0.9976,
+        price: 0,
+        quantity: 0,
+        action: 'sell',
+        symbol: 'ETH_BRL',
+        quote: 'ETH',
+        base: 'BRL',
+        total: 0,
+        trade: 'ETH',
+        receive: null,
+        transfer: null
+      }
+    ]
+  },
 
   {
     name: 'BTC_BRL >> BTC_USDT >> USDT_BRL',
